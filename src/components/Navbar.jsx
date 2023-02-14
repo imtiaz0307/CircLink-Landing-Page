ef608cfd629c363b7b5288fa1fbde6ddbb1a2319
imtiaz0307 <imtiaz0307@gmail.com>
2023-02-14 14:59:10 +0500

    worked on css, more specifically on responsiveness of the page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import styles from './styles/Navbar.module.css'
 const Navbar = ({ hero, feature, about, contact }) => {
     const scrollToSection = (section) => section.scrollIntoView()
     const [sticky, setSticky] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
 
     useEffect(() => {
         const observer = new IntersectionObserver((entires) => setSticky(!entires[0].isIntersecting),
@@ -21,11 +22,28 @@ const Navbar = ({ hero, feature, about, contact }) => {
             <p className={styles.logo}>
                 <a href='#'>CircLink</a>
             </p>
-            <ul className={styles.mainMenu}>
-                <li><a href='#'>Home</a></li>
-                <li onClick={() => scrollToSection(feature.current)}>Features</li>
-                <li onClick={() => scrollToSection(about.current)}>About</li>
-                <li onClick={() => scrollToSection(contact.current)}>Contact Us</li>
+            <div className={styles.hamburger} onClick={() => setShowMenu(!showMenu)}>
+                <div className={styles.line} style={{ transform: `translateY(${showMenu ? "3px" : 0}) rotate(${showMenu ? '45deg' : '0deg'})` }}></div>
+                <div className={styles.line} style={{ display: showMenu ? 'none' : 'block' }}></div>
+                <div className={styles.line} style={{ transform: `translateY(${showMenu ? "-4px" : 0}) rotate(${showMenu ? '-45deg' : '0deg'})` }}></div>
+            </div>
+            <ul className={`${styles.mainMenu} ${showMenu && styles.showMenu}`}>
+
+                <li><a href='#' onClick={() => setShowMenu(!showMenu)}>Home</a></li>
+
+                <li onClick={() => {
+                    setShowMenu(!showMenu)
+                    scrollToSection(feature.current)
+                }}>Features</li>
+
+                <li onClick={() => {
+                    setShowMenu(!showMenu)
+                    scrollToSection(about.current)
+                }}>About</li>
+                <li onClick={() => {
+                    setShowMenu(!showMenu)
+                    scrollToSection(contact.current)
+                }}>Contact Us</li>
                 <li><a target="_blank" href='https://circlink.up.railway.app/'>Go To Api &rarr;</a></li>
             </ul>
         </nav>
